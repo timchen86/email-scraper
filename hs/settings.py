f432c9558a1388c80d7741b2d80a193c8ae7d6e8
timchen86 <tim.chen.86@gmail.com>
2012-12-26 00:04:39 +0800

    only parse email from the related domain

--- hs/settings.py
+++ hs/settings.py
@@ -5,11 +5,18 @@
 #
 #     http://doc.scrapy.org/topics/settings.html
 #
+import os
 
 BOT_NAME = 'hs'
 
 SPIDER_MODULES = ['hs.spiders']
 NEWSPIDER_MODULE = 'hs.spiders'
 
+RAW_JSON = os.path.expanduser('~/%s_raw.json' % BOT_NAME) 
+SORTED_JSON = os.path.expanduser('~/%s.json' % BOT_NAME)
+
+FEED_URI = RAW_JSON
+FEED_FORMAT = "JSON"
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'hs (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
--- hs/spiders/hs_spider.py
+++ hs/spiders/hs_spider.py
@@ -8,24 +8,28 @@ from scrapy.xlib.pydispatch import dispatcher
 
 import hs.settings as settings
 
+from urlparse import urlparse
+import json
 import copy
 import re
 import csv
 import datetime 
+import pprint 
 
 class HsItem(Item):
     # define the fields for your item here like:
+    domain = Field()
     mail = Field()
-    mailto = Field()
-    link = Field()
 
 class HsSpider(BaseSpider):
     name = "hs"
     #allowed_domains = ["www.missingkids.org.tw"]
     start_urls = ["http://www.nssh.ntpc.edu.tw/bin/home.php"]
 
+    i = 0
+
     def __init__(self):
-        #dispatcher.connect(self.engine_stopped, signals.engine_stopped)
+        dispatcher.connect(self.engine_stopped, signals.engine_stopped)
         pass
 
 
@@ -34,8 +38,16 @@ class HsSpider(BaseSpider):
 
         urls = hxs.select('//a/@href').extract()
 
+        domain = urlparse(response.url).netloc
+        ds = domain.split('.')
+        base1 = ds[len(ds)-2]
+        base2 = ds[len(ds)-3]
+
         for u in urls:
-            yield Request(u, self.mail_parser)
+            if base1 in u or base2 in u:
+                self.i+=1
+                if self.i<300:
+                    yield Request(u, self.mail_parser)
 
 
     def mail_parser(self, response):
@@ -43,8 +55,29 @@ class HsSpider(BaseSpider):
         item = HsItem()
         mailto = hxs.select("//a[@href]").re("mailto:(.*?)[,\"%\?]{1}")
         mail = mailto + hxs.select("//text()").re("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}")
-        
+ 
+        domain = urlparse(response.url).netloc.split('.')
+        item['domain'] = '.'.join(domain[(len(domain)-4):])
         item['mail'] = list(set(mail))
-        
+
         if len(item['mail'])>0:
             return item
+
+        
+    def engine_stopped(self):
+        with open(settings.RAW_JSON,"rb") as json_file:
+            data_raw = json.load(json_file)
+               
+            data = []
+
+            for dr in data_raw:
+                if dr["domain"] not in [s["domain"] for s in data]:
+                    data.append(dr)
+                else:
+                    for d in data:
+                        if d["domain"] == dr["domain"]:
+                            d["mail"] += dr["mail"]
+                        d["mail"] = list(set(d["mail"]))    
+                        
+            with open(settings.SORTED_JSON, 'wb') as json_file:
+                json.dump(data, json_file)
